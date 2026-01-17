187287c170d53bf2acb88e021115d98960848a9b
xtopint00 <tadeas.topinka@gmail.com>
2026-01-17 15:22:18 +0100

    [FEATURE] started work on textbox base to allow the user to edit row names and for them to properly linebreak instead of becoming unreadable

---
+++ Widgets/TextBoxBase.py
@@ -0,0 +1,44 @@
+from Common.PlaceholderTexts import PlaceholderTexts
+from WidgetsBase import *
+
+class TextBoxBase(CTkEntry):
+    def __init__(self,
+                 master: CTkFrame,
+                 border_color = "401c1b",
+                 fg_color = "transparent",
+                 text = PlaceholderTexts.DESCRIPTION,
+                 text_color = MXB_RED,
+                 wrap = "word",
+                 height = 35,
+                 activate_scrollbars = False,
+                 border_width = 0,
+                 *args,
+                 **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.configure(border_color=border_color,
+                       fg_color=fg_color,
+                       text_color=text_color,
+                       wrap = wrap,
+                       height = height,
+                       activate_scrollbars = activate_scrollbars,
+                       border_width = border_width)
+        self.insert(0.0, text)
+
+        self.configure(state = "disabled")
+        self.editing = False
+
+        self.bind("<Button-1>", self.start_edit)
+        self.bind("<Return>", self.stop_edit)
+        self.bind("<FocusOut", self.stop_edit)
+        self.bind("<Shift-Return>", self.add_newline)
+
+    def start_edit(self, event = None) -> None:
+        self.configure(state = "normal")
+        self.focus_set()
+
+    def stop_edit(self, event = None):
+        self.configure(state = "disabled")
+        return "break"
+
+    def add_newline(self, event = None) -> None:
+        pass
