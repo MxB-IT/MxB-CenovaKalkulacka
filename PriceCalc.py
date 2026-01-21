054e0ddf7349053995115df89ab06c1bb83db2fc
Lander456 <tedus.lp@gmail.com>
2026-01-21 02:31:09 +0100

    [FEATURE] redid rendering of the entire main window to speed  it up, debouncing frame updates while dragging so it does not lag as much as it did before, plus stopped using scrollable frame, that is less efficient than a base tkinter scrollbar attached to a canvas

--- PriceCalc.py
+++ PriceCalc.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from Sections.BaseSection import *
 from Sections.AccountingSection import *
 from Sections.PayrollSection import *
@@ -15,19 +16,42 @@ class PriceCalc(CTk):
         super().__init__()
         self.geometry("400x600")
         self.title("Cenová kalkulačka")
-        self.scrollable_frame = CTkScrollableFrame(master=self,
-                                                   border_width = 2,
-                                                   border_color = "white")
-        self.scrollable_frame.grid(row=0,
-                                   column=0,
-                                   padx=10,
-                                   pady=10,
-                                   sticky="nsew")
+        self._resize_timer = None
 
-        self.grid_rowconfigure(index=0,
+        self.grid_rowconfigure(0,
                                weight=1)
-        self.grid_columnconfigure(index=0,
+        self.grid_columnconfigure(0,
                                   weight=1)
+
+        self.canvas = tk.Canvas(self,
+                                borderwidth=0,
+                                highlightthickness=0,
+                                bg="white")
+        self.canvas.grid(row=0,
+                         column=0,
+                         sticky="nsew")
+
+        self.scrollbar = tk.Scrollbar(self,
+                                      orient="vertical",
+                                      command=self.canvas.yview)
+        self.scrollbar.grid(row=0,
+                            column=1,
+                            sticky="ns")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.scrollable_frame = FrameBase(master=self.canvas,
+                                          border_width = 2,
+                                          border_color = "white")
+
+        self.canvas_window = self.canvas.create_window((0, 0),
+                                                       window=self.scrollable_frame,
+                                                       anchor="nw")
+
+        self.scrollable_frame.bind("<Configure>", self._on_frame_configure)
+        self.canvas.bind("<Configure>", self._on_canvas_configure)
+        self.canvas.bind("<Configure>", self._on_canvas_configure)
+        self.bind("<Enter>", self._bind_mouse_scroll)
+        self.bind("<Leave>", self._unbind_mouse_scroll)
 
         self.base_section = BaseSection(self.scrollable_frame, self)
         self.total_section = TotalSection(self.scrollable_frame)
@@ -51,21 +75,59 @@ class PriceCalc(CTk):
                                       column = 0,
                                       columnspan = 3,
                                       sticky = "ew")
-        self.update()
         self.scrollable_frame.configure(width = self.winfo_width() - 40,
                                         height = self.winfo_height() - 40)
         self.scrollable_frame.rowconfigure((0,4), weight = 1)
         self.scrollable_frame.columnconfigure(0, weight = 1)
 
+    def _bind_mouse_scroll(self, event) -> None:
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+        self.canvas.bind_all("<Button-4>", self._on_mousewheel)
+        self.canvas.bind_all("<Button-5>", self._on_mousewheel)
+
+    def _unbind_mouse_scroll(self, event) -> None:
+        self.canvas.unbind_all("<MouseWheel>")
+        self.canvas.unbind_all("<Button-4>")
+        self.canvas.unbind_all("<Button-5>")
+
+    def _on_frame_configure(self, event=None):
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+
+    def _on_canvas_configure(self, event):
+
+        if self._resize_timer:
+            self.after_cancel(self._resize_timer)
+
+        self._resize_timer = self.after(150, lambda: self._perform_resize(event.width))
+
+    def _perform_resize(self, new_width: int) -> None:
+        self.canvas.itemconfig(self.canvas_window,
+                               width=new_width)
+
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+        self._resize_timer = None
+
+    def _on_mousewheel(self, event):
+        if event.delta:
+            self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+        elif event.num == 4:
+            self.canvas.yview_scroll(-1, "units")
+        elif event.num == 5:
+            self.canvas.yview_scroll(1, "units")
+
     def calculate_total(self, *args) -> None:
         """
-        calculates the totals based on all the input data, calls itself every 100ms (subject to change)
+        calculates the totals based on all the input data, called upon changes in totals
         :return: None
         """
         try:
-            self.total_section.payrolls_total[1].configure(text = NUMBER_FORMAT % self.payrolls_section.total.get())
-            self.total_section.accounting_total[1].configure(text = NUMBER_FORMAT % self.accounting_section.total.get())
-            self.total_section.total_price[1].configure(text = NUMBER_FORMAT % (self.payrolls_section.total.get() + self.accounting_section.total.get()))
+            payrolls_total = self.payrolls_section.total.get()
+            accounting_total = self.accounting_section.total.get()
+            combined_total = payrolls_total + accounting_total
+
+            self.total_section.payrolls_total[1].configure(text = NUMBER_FORMAT % payrolls_total)
+            self.total_section.accounting_total[1].configure(text = NUMBER_FORMAT % accounting_total)
+            self.total_section.total_price[1].configure(text = NUMBER_FORMAT % combined_total)
         except AttributeError:
             pass
 
