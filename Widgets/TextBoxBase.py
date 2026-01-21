d3cb082182a638fc8fe27bdfc57827d68d1478ac
Lander456 <tedus.lp@gmail.com>
2026-01-21 02:30:09 +0100

    [FEATURE] made sure the textbox activates on a double click instead of a single click to ensure a user does not accidentally edit something, when editing the textbox displays a frame and a different background as well

--- Widgets/TextBoxBase.py
+++ Widgets/TextBoxBase.py
@@ -6,12 +6,12 @@ class TextBoxBase(CTkTextbox):
                  master: CTkFrame,
                  border_color = "401c1b",
                  fg_color = "transparent",
-                 text = PlaceholderTexts.DESCRIPTION,
                  text_color = MXB_RED,
                  wrap = "word",
                  height = 35,
                  activate_scrollbars = False,
                  border_width = 0,
+                 text = PlaceholderTexts.DESCRIPTION,
                  *args,
                  **kwargs):
         super().__init__(master,
@@ -24,23 +24,47 @@ class TextBoxBase(CTkTextbox):
                          border_width=border_width,
                          *args,
                          **kwargs)
+        self._resize_timer = None
+
         self.insert(0.0, text)
+
+        self.after(100, self.update_height)
 
         self.configure(state = "disabled")
         self.editing = False
 
-        self.bind("<Button-1>", self.start_edit)
+        self.bind("<Double-Button-1>", self.start_edit)
         self.bind("<Return>", self.stop_edit)
         self.bind("<FocusOut>", self.stop_edit)
         self.bind("<Shift-Return>", self.add_newline)
+        self.bind("<Configure>", self.on_resize)
 
     def start_edit(self, event = None) -> None:
-        self.configure(state = "normal")
+        self.configure(state="normal",
+                       border_color=MXB_RED,
+                       border_width=2,
+                       fg_color="#b39998")
         self.focus_set()
 
     def stop_edit(self, event = None):
-        self.configure(state = "disabled")
+        self.configure(state="disabled",
+                       border_width=0,
+                       fg_color="white")
         return "break"
 
     def add_newline(self, event = None) -> None:
         pass
+
+    def update_height(self):
+        num_lines = self._textbox.count("1.0", "end", "displaylines")[0]
+
+        new_height = num_lines
+
+        if self.cget("height") != new_height:
+            self.configure(height=new_height)
+
+    def on_resize(self, event):
+        if self._resize_timer:
+            self.after_cancel(self._resize_timer)
+
+        self._resize_timer = self.after(200, self.update_height)
