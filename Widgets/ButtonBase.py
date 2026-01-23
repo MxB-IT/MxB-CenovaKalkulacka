a698b1cc622d1f471e4d37b6cd8eb7e411a0a1e5
xtopint00 <tadeas.topinka@gmail.com>
2026-01-23 20:59:17 +0100

    [FEATURE] started work on a new button class to use for inline adding of new rows

---
+++ Widgets/ButtonBase.py
@@ -0,0 +1,16 @@
+from Widgets.WidgetsBase import *
+
+class ButtonBase(CTkButton):
+    def __init__(self,
+                 master: CTkFrame,
+                 fg_color=MXB_RED,
+                 border_color=MXB_RED,
+                 text_color="white",
+                 *args,
+                 **kwargs):
+        super().__init__(*args,
+                         master=master,
+                         fg_color=fg_color,
+                         border_color=border_color,
+                         text_color=text_color,
+                         **kwargs)
